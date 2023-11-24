b1d8b7038b9b8e397e1e908c78d925cb8e5458d8
Duthaegaux <wrrr000@mail.ru>
2023-11-24 20:59:45 +0300

    Жоска починили регистрацию/вход на сайт

--- register.py
+++ register.py
@@ -2,24 +2,20 @@ from flask import *  # подключили библиотеку flask
 # from flask_login import *
 from werkzeug.exceptions import BadRequestKeyError
 from database import *
-app = Flask(__name__)
-# создали фласк приложение(веб-сервер)
+app = Flask(__name__) # создали фласк приложение(веб-сервер)
 
 
 def remain():
     if request.cookies.get('logged'):
         return redirect("/main_page")
-    print("To setcookie -->>")
     return setcookie()
 
 
 def setcookie():
     try:
         if db.in_db(request.form["login"]):
-            res = make_response()
+            res = make_response(render_template("example.html"))
             res.set_cookie('logged', 'yes', 1296000)
-            print("To main_page -->>")
-            redirect("/main_page")
             return res
         else:
             return redirect('/register')
@@ -36,25 +32,18 @@ def register():
 
 @app.route('/main_page', methods=['GET', 'POST'])
 def main_page():
-    print("In main page")
     if request.cookies.get('logged'):
-        print("Here 1")
         return render_template('example.html')
     try:
         if not db.in_db(request.form["login"]):
-            print("Here 2")
             db.insert_user(request.form["login"], request.form["password"])
-            print("To remain -->>")
             return remain()
         else:
             if db.check_password(request.form["login"], request.form["password"]):
-                print("Here 3")
                 return remain()
             else:
-                print("Here 4")
                 return redirect('/register')
     except BadRequestKeyError:
-        print("Here 5")
         return redirect('/register')
 
 
